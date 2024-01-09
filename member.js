54ea0f10eb5400a8062f82346810e91f46714ec7
Shreyas-JA <112689308+Shreyas-JA@users.noreply.github.com>
2024-01-09 12:08:50 +0000

    Copilot second commit

---
+++ member.js
@@ -0,0 +1,9 @@
+function skillsMember() {
+  var skills = ['HTML', 'CSS', 'JS', 'React', 'Node'];
+  var member = {
+    name: 'Samantha',
+    age: 25,
+    skills: skills
+  };
+  return member.skills[3];
+}
